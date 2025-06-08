7b8f65de546879218d7fbcb212cb87218eca620a
Shashank S <shashank5418shashu@gmail.com>
2025-06-08 18:23:20 +0530

    made a card compenents using props

--- 03tailwind/src/App.jsx
+++ 03tailwind/src/App.jsx
@@ -9,7 +9,9 @@ function App() {
     
     <h1 className="text-3xl font-bold underline" >Working on tailwind</h1>
 
-    <Card/>
+    <Card img ="https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D"  name = "Shashank"/>
+    <Card img ="https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D"  name = "Shashu"/>
+    <Card img ="https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D"  name = "Shreyas"/>
 
     </>
   )
--- 03tailwind/src/components/Card.jsx
+++ 03tailwind/src/components/Card.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 
-const Card = () => {
+const Card = ({img , name}) => {
   return (
     <div class="max-w-sm mx-auto p-6 bg-white shadow-xl rounded-2xl space-y-4 text-center">
   <img 
     class="w-full h-64 object-cover rounded-xl"  
-    src="https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D" 
+    src= {img}
     alt="Profile image" 
   />
-  <h1 class="text-2xl font-bold text-gray-800">Shashank S</h1>
+  <h1 class="text-2xl font-bold text-gray-800">{name}</h1>
   <p class="text-gray-600 text-base">Lorem, ipsum. lorem6</p>
 </div>
 
