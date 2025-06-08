31c31732d968660fd02414ae5286bf9c7169d13a
Shashank S <shashank5418shashu@gmail.com>
2025-06-08 19:30:48 +0530

    Some Changes made

--- 05Password/src/App.jsx
+++ 05Password/src/App.jsx
@@ -4,47 +4,58 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [length, setlength] = useState(10);
+  const [length, setlength] = useState(6);
   const [character, setCharacter] = useState(false);
   const [number, setNumber] = useState(false);
+  const [password , setPassword] = useState('')
 
   return (
     <>
-      <div class="max-w-md mx-auto mt-10 p-6 bg-white rounded-2xl shadow-md space-y-6">
-       <div class="flex">
+      <div className="max-w-md mx-auto mt-10 p-6 bg-white rounded-2xl shadow-md space-y-6">
+       <div className="flex">
     <input
       type="text"
-      placeholder="Enter text"
-      class="flex-grow p-2 border border-gray-300 rounded-l-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+      value={password}
+      readOnly
+      placeholder="Password"
+      className="flex-grow p-2 border border-gray-300 rounded-l-md focus:outline-none focus:ring-2 focus:ring-blue-500"
     />
     <button
-      class="px-4 bg-blue-500 text-white font-medium rounded-r-md hover:bg-blue-600 transition"
+      className="px-4 bg-blue-500 text-white font-medium rounded-r-md hover:bg-blue-600 transition"
     >
       Copy
     </button>
   </div>
 
-        <div class="space-y-2">
-          <label for="range" class="block text-gray-700 font-medium">
-            Length
+        <div className="space-y-2">
+          <label htmlFor="range" className="block text-gray-700 font-medium">
+            Length : {length}
           </label>
-          <input type="range" id="range" class="w-full accent-blue-500" />
+          <input 
+          type="range" 
+          id="range"
+           className="w-full accent-blue-500"
+            min={6}
+             max={20}
+              value={length}
+              onChange={(e)=> setlength(e.target.value)}
+              />
         </div>
 
-        <div class="flex items-center space-x-3">
+        <div className="flex items-center space-x-3">
           <input
             type="checkbox"
             id="character"
-            class="w-5 h-5 accent-blue-500"
+            className="w-5 h-5 accent-blue-500"
           />
-          <label for="character" class="text-gray-700">
+          <label htmlFor="character" className="text-gray-700">
             Character
           </label>
         </div>
 
-        <div class="flex items-center space-x-3">
-          <input type="checkbox" id="number" class="w-5 h-5 accent-blue-500" />
-          <label for="number" class="text-gray-700">
+        <div className="flex items-center space-x-3">
+          <input type="checkbox" id="number" className="w-5 h-5 accent-blue-500" />
+          <label htmlFor="number" className="text-gray-700">
             Number
           </label>
         </div>
