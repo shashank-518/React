febb74729ce10f2f0865af4506e6a5b3c7272920
Shashank S <shashank5418shashu@gmail.com>
2025-06-09 17:03:12 +0530

    Input Box

--- 06Currency/src/App.jsx
+++ 06Currency/src/App.jsx
@@ -5,7 +5,17 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <h1 className='bg-red-600' >Cuurency Project</h1>
+
+     <div
+      className="h-screen bg-cover bg-center flex items-center justify-center"
+      style={{
+        backgroundImage:
+          "url('https://images.pexels.com/photos/4497591/pexels-photo-4497591.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')",
+      }}
+    >
+      <h1 className=" bg-red-600 text-white text-4xl font-bold">Hello World</h1>
+    </div>
+
   )
 }
 
