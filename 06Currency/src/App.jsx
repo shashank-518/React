d62588244681501fa3f764b67a16870822356c65
Shashank S <shashank5418shashu@gmail.com>
2025-06-09 17:40:58 +0530

    Done with currency

--- 06Currency/src/App.jsx
+++ 06Currency/src/App.jsx
@@ -1,21 +1,88 @@
 import { useState } from 'react'
+import InputBox from './components/Inputbox'
+import useCurrencyinfo from './hooks/useCurrencyinfo'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [amount , setamount] = useState(0)
+  const [converted , setconverted] = useState(0)
+  const [from , setfrom] = useState("usd")
+  const [to , setto] = useState("inr")
+
+  const currencyinfo = useCurrencyinfo(from)
+
+  console.log(currencyinfo);
+  
+
+  const options = Object.keys(currencyinfo)
+
+  const swap = ()=>{
+    setto(from)
+    setfrom(to)
+    setamount(converted)
+    setconverted(amount)
+  }
+
+  const change = ()=>{
+      setconverted(amount * currencyinfo[to])
+  }
+
+
+
 
   return (
-
-     <div
-      className="h-screen bg-cover bg-center flex items-center justify-center"
-      style={{
-        backgroundImage:
-          "url('https://images.pexels.com/photos/4497591/pexels-photo-4497591.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')",
-      }}
-    >
-      <h1 className=" bg-red-600 text-white text-4xl font-bold">Hello World</h1>
-    </div>
-
+    <div
+            className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
+            style={{
+                backgroundImage: `url('${'https://images.pexels.com/photos/261102/pexels-photo-261102.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'})`,
+            }}
+        >
+            <div className="w-full">
+                <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
+                    <form
+                        onSubmit={(e) => {
+                            e.preventDefault();
+                           change()
+                        }}
+                    >
+                        <div className="w-full mb-1">
+                            <InputBox
+                                label="From"
+                                amount = {amount}
+                                onAmountChange={(amount) => (setamount(amount))}
+                                currencyOptions = {options} 
+                                onCurrencyChange={(currency) => (setfrom(currency))}
+                                selectCurrency={from}
+                            />
+                        </div>
+                        <div className="relative w-full h-0.5">
+                            <button
+                                type="button"
+                                className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5"
+                                onClick={swap}
+                            >
+                                swap
+                            </button>
+                        </div>
+                        <div className="w-full mt-1 mb-4">
+                            <InputBox
+                                label="To"
+                                amount = {converted}
+                                onAmountChange={(amount) => (setconverted(amount))}
+                                currencyOptions = {options} 
+                                onCurrencyChange={(currency) => (setto(currency))}
+                                selectCurrency={to}
+                                
+                            />
+                        </div>
+                        <button type="submit" className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg">
+                            Convert 
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
   )
 }
 
--- 06Currency/src/hooks/useCurrencyinfo.jsx
+++ 06Currency/src/hooks/useCurrencyinfo.jsx
@@ -1,7 +1,19 @@
 import {useState , useEffect} from 'react'
 
 
-const useCurrencyinfo = ()=>{
+const useCurrencyinfo = (currency)=>{
+
+    const [data , setData] = useState({})
+
+    useEffect(()=>{
+        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2024-03-06/v1/currencies/${currency}.json`)
+        .then((res)=>res.json())
+        .then((res)=>setData(res[currency]))
+    },[currency])
+
+    console.log(data);
+    return data
+    
 
 }
 
