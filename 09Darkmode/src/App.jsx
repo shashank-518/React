64e084aa2ce953b59c546af9885fa6537d793cc2
Shashank S <shashank5418shashu@gmail.com>
2025-06-12 00:05:06 +0530

    Components made successfully

--- 09Darkmode/src/App.jsx
+++ 09Darkmode/src/App.jsx
@@ -2,12 +2,20 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import ThemeBtn from './Components/Toggle'
+import Card from './Components/Card'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
+    <>
     <h1 className='bg-red-100' >DArk Mode Toggle</h1>
+
+    <ThemeBtn/>
+    <Card/>
+    
+    </>
   )
 }
 
