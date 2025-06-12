07f69f53134b9e29fd2a8f81c51746f31f406add
Shashank S <shashank5418shashu@gmail.com>
2025-06-12 13:25:50 +0530

    Two Functions Completed

--- 10Todo/src/App.jsx
+++ 10Todo/src/App.jsx
@@ -2,12 +2,26 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { TodoContextProvider } from './Contexts/TodoContext'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [Todos, setTodos] = useState([])
+
+  const addTodo = (todo)=>{
+    setTodos((prev)=> [ {id: Date.now() , ...todo} ,...prev])
+  }
+
+  const updateTodo = (id,todo)=>{
+    setTodos((prev) => prev.map((prevTodo)=> (prevTodo.id === id  ? todo : prevTodo)) )
+  }
 
   return (
-    <h1 className='bg-red-800' >Todo App</h1>
+
+    <TodoContextProvider value={{Todos , addTodo ,updateTodo ,deleteTodo , toggleTodo}} >
+
+      <h1 className='bg-red-800' >Todo App</h1>
+    </TodoContextProvider>
+
   )
 }
 
