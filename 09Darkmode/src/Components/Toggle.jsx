76c32ccf3c781b284b8041648830ca4ae3526053
Shashank S <shashank5418shashu@gmail.com>
2025-06-12 10:07:12 +0530

    Everything is done

--- 09Darkmode/src/Components/Toggle.jsx
+++ 09Darkmode/src/Components/Toggle.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { themeApi } from '../Context/Theme';
 
 function ThemeBtn() {
 
-    const onChangeBtn = ()=>{
+    const {ThemeValue , darkTheme ,lightTheme} = themeApi()
+
+    const onChangeBtn = (e)=>{
+
+        const themevalueStatus = e.currentTarget.checked
+
+        if(themevalueStatus){
+            darkTheme()
+        }else{
+            lightTheme()
+        }
         
     }
     
@@ -17,6 +28,7 @@ function ThemeBtn() {
                 value=""
                 className="sr-only peer"
                 onChange={onChangeBtn}
+                checked = {ThemeValue==='dark'}
             />
             <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
             <span className="ml-3 text-sm font-medium text-gray-900">Toggle Theme</span>
