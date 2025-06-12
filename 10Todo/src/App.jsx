a8370ef857e7a8e6b9dc82ae615e588c4e97e686
Shashank S <shashank5418shashu@gmail.com>
2025-06-12 14:35:54 +0530

    React Completed

--- 10Todo/src/App.jsx
+++ 10Todo/src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import { TodoContextProvider } from './Contexts/TodoContext'
 import TodoForm from './Components/TodoForm'
+import TodoItem from './Components/TodoItem'
 
 function App() {
   const [Todos, setTodos] = useState([])
@@ -41,7 +42,26 @@ function App() {
 
     <TodoContextProvider value={{Todos , addTodo ,updateTodo ,deleteTodo , toggleTodo}} >
 
-      <TodoForm/>
+     <div className="bg-[#172842] min-h-screen py-8">
+                <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+                    <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+                    <div className="mb-4">
+                        
+                        <TodoForm />
+                    </div>
+                    <div className="flex flex-wrap gap-y-3">
+                        {/*Loop and Add TodoItem here */}
+                        {Todos.map((todo) => (
+                          <div key={todo.id}
+                          className='w-full'
+                          >
+                            <TodoItem todo={todo} />
+                          </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
+
     </TodoContextProvider>
 
   )
